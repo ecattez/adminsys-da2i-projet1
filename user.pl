529dd465215b6573224eafd8707a499a5318ccb5
Edouard CATTEZ <edouard.cattez@sfr.fr>
2016-01-14 12:16:24 +0100

    ajout/suppression des utilisateurs

--- user.pl
+++ user.pl
@@ -17,10 +17,24 @@
 # -m (ou --modify)	[value] : modifie 1 à n utilisateurs
 
 use strict;
-use Getopt::Std;
+use Getopt::Long;
 use Time::HiRes qw(time);
+use FindBin '$Bin';
+use Digest::SHA qw(sha512);
 
-my $usage = "Usage: $0 <OPTIONS> [VALUES...]\n";
+my $root	= $Bin;
+my $usage	= "Usage: $0 <OPTIONS> [VALUES...]\n";
+my $upass	= 'upass';
+my $passwd	= 'passwd';
+my $shadow	= 'shadow';
+my $group	= 'group';
+my ($h, $n, @a, @r, @m);
+GetOptions('h' => \$h, 'n' => \$n,'a=s@' => \@a,'r=s@' => \@r,'m=s@{4}' => \@m);
+
+sub crypt_password
+{
+	return unpack("H*", sha512($_[0]));
+}
 
 sub print_usage
 {
@@ -42,27 +56,157 @@ sub show_help
 	print_opt "-m (ou --modify) <value>", "modifie 1 à n utilisateurs";
 }
 
+sub get_available_uid
+{
+	my $uid = 1000;
+	while (getpwuid($uid))
+	{
+		$uid++;
+	}
+	return $uid;
+}
+
+sub get_available_gid
+{
+	my $gid = 1000;
+	while (getgrgid($gid))
+	{
+		$gid++;
+	}
+	return $gid;
+}
+
+sub get_default_home
+{
+	my $home = $root . '/home/';
+	mkdir $home unless -d $home;
+	return $home . $_[0];
+}
+
+sub get_default_shell
+{
+	return '/bin/bash';
+}
+
+sub get_random_password
+{
+	return crypt(srand, 'random');
+}
+
+sub print_in_file
+{
+	my ($file, $content) = @_;
+	open FILE, '>>', $file or die "open: $!";
+	print FILE $content, "\n";
+	close FILE or die "close: $!";
+}
+
+# <login>
 sub create_user
 {
-	my ($login, $password, $uid, $gid, $name, $home, $shell) = @_;
-	open PASSWD, '>>passwd' or die "open: $!";
-	print PASSWD "$login:x:$uid:$gid:$name:$home:$shell\n";
-	close PASSWD or die "close: $!";
+	my ($login, $home) = ($_[0], get_default_home($_[0]));
+	my ($password, $uid, $gid, $shell) = (get_random_password(), get_available_uid(), get_available_gid(), get_default_shell());
 	
-	my $password_hash = crypt $password, "\$6\$Ay4p\$";
-	open SHADOW, '>>shadow' or die "shadow: $!";
-	print SHADOW "$login:$password_hash:" . time . ":0:99999:7:::\n";
-	close SHADOW or die "close: $!";
+	# 1) Ecrit dans un fichier le couple login/mdp pour l'admin sys et les utilisateurs
+	# 2) Ecrit dans le fichier passwd
+	# 3) Ecrit dans le fichier shadow (le password est crypté)
+	# 4) Ecrit dans le fichier group
+	my %hash;
+	$hash{$upass} = "$login:$password";
+	$hash{$passwd} = "$login:x:$uid:$gid::$home:$shell";
+	$hash{$shadow} = $login . ':' . crypt_password($password) . ':' . time . ':0:99999:7:::';
+	$hash{$group} = "$login:x:$gid:";
+	
+	while (my ($key, $value) = each %hash)
+	{
+		print_in_file($key, $value);
+	}
+	
+	# Création du dossier personnel de l'utilisateur
+	mkdir $home or die "mkdir: $!";
+	# Mise en place des fichiers d'initialisation du shell
+	#system("cp -v /etc/skel/.bash* $home");
+	# Attribution des droits
+	#system("chmod -v 0755 $home");
+	# Définir le propriétaire
+	#system("chown -vR $login:$login $home");
 }
 
+sub remove_user_from_file
+{
+	my ($file, $login) = @_;
+	my @list;
+	
+	# Supprimer dans le fichier passwd
+	open FILE, '<', $file or die "open: $!";
+	while (<FILE>)
+	{
+		push(@list, $_) unless (/^$login/);
+	}
+	close FILE or die "close: $!";
+	
+	open FILE, '>', $file or die "open: $!";
+	print FILE @list;
+	close FILE or die "close: $!";
+}
+
+# <login>
 sub remove_user
 {
-
+	my $login = $_[0];
+	my @files = ($passwd, $shadow, $group);
+	foreach (@files)
+	{
+		remove_user_from_file($_, $login);
+	}
 }
 
+# <login, password, home, shell>
 sub modify_user
 {
 
 }
 
-create_user @ARGV;
+# Test de commande sans effet
+sub dry_run
+{
+
+}
+
+if ($h)
+{
+	show_help();
+}
+elsif ($n)
+{
+
+}
+elsif (@a)
+{
+	foreach (@a)
+	{	
+		create_user $_;
+	}
+}
+elsif (@r)
+{
+	foreach (@r)
+	{
+		remove_user $_;
+	}
+}
+elsif (@m)
+{
+	foreach (@m)
+	{
+		modify_user $_;
+	}
+}
+else
+{
+	while(<STDIN>)
+	{
+		chomp;
+		create_user $_;
+	}
+}
